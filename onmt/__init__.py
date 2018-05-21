959bcff05d78352f0601c9641f09ff2e3530266e
Tong <georgefusmail@gmail.com>
2018-05-21 23:41:49 +0800

    for pycharm reading

--- onmt/__init__.py
+++ onmt/__init__.py
@@ -5,6 +5,11 @@ import onmt.Loss
 from onmt.Trainer import Trainer, Statistics
 from onmt.Optim import Optim
 
+# add by NO42.
+# for pycharm reading.
+import onmt.modules
+
+
 # For flake8 compatibility
 __all__ = [onmt.Loss, onmt.Models,
            Trainer, Optim, Statistics, onmt.io, onmt.translate]
